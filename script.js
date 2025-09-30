3d31801346ba97295102f67e53147468856edc0c
sadikh0903 <sadikh2789@nycstudents.net>
2025-09-30 19:52:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Sadik Hossain"; // update the part BETWEEN the "quotes"
+let userName = "sadikh0903"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
